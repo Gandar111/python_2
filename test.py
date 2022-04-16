beafe63f4110d254cdaf4f35347308713ce4effb
Gandar111 <101665553+Gandar111@users.noreply.github.com>
2022-04-16 19:30:20 +0200

    kjk

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ words={"a", "b", "c","e"}
 nummers={1,12,33,5,6,77}
 def methode(word):
      multi= 2*word
-     return multi/2
+     return multi/2-1
      
 erg= [ methode(word) for word in nummers]
 print(erg)
