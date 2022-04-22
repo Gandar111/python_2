634ceeca0eb38769e6e4058879533190774b2e29
Gandar111 <101665553+Gandar111@users.noreply.github.com>
2022-04-22 21:19:07 +0200

    intit bnbn

--- test.py
+++ test.py
@@ -1,8 +1,2 @@
-class newclass():
-     class_variable=3
-     def methode(self, attibutte):
-          self.class_variable=attibutte
-          return self.class_variable*2
-projekt= newclass()
-l=projekt.methode(28)
-print(l)
+import pandas 
+print("hijjjjjjjjj,,jjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj")
